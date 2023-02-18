4f625bb9a0196d268e84669b9f1fd498a1239e2e
FrancescoPenasa <francesco.penasa.job@gmail.com>
2023-02-18 21:04:31 +0100

    add /cast endpoint + template

--- cast/views.py
+++ cast/views.py
@@ -1,4 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Person, Movie, Character
 
 def index(request):
-    return HttpResponse("Hello, world. cast API.")
+    latest_movie_list = Movie.objects.order_by('-updated_at')[:5]
+    context = {
+        'latest_movie_list': latest_movie_list,
+    }
+    return render(request, 'cast/index.html', context)
